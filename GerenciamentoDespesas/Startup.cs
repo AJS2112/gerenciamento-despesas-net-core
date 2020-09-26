e130c7e13c9c66ad4a77cc2a3f3ab9e0bbce9551
Antonio Sucre <ajs2104@gmail.com>
2020-09-26 01:41:37 -0300

    CRUD Tipos de Despesas

--- GerenciamentoDespesas/Startup.cs
+++ GerenciamentoDespesas/Startup.cs
@@ -60,7 +60,7 @@ namespace GerenciamentoDespesas
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=TipoDespesas}/{action=Index}/{id?}");
             });
         }
     }
