77ae676677f149d464986a6699ec41e2057288f9
Antonio Sucre <ajs2104@gmail.com>
2020-09-27 20:40:50 -0300

    Viewcomponent para Estatisticas

---
+++ GerenciamentoDespesas/ViewComponents/EstatisticasViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using GerenciamentoDespesas.Models;
+using GerenciamentoDespesas.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciamentoDespesas.ViewComponents
+{
+    public class EstatisticasViewComponent: ViewComponent
+    {
+        private readonly Contexto _context;
+
+        public EstatisticasViewComponent(Contexto context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            EstatisticasViewModel estatisticas = new EstatisticasViewModel();
+
+            estatisticas.MenorDespesa= _context.Despesas.Min(x => x.Valor);
+            estatisticas.MaiorDespesa = _context.Despesas.Max(x => x.Valor);
+            estatisticas.QuantidadeDespesas = _context.Despesas.Count();
+
+            return View(estatisticas);
+        }
+    }
+}
